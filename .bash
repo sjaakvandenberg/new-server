80393e88599965d2c897e2bab494a6e357f0cf39
sjaakvandenberg <mail@svdb.co>
2016-02-07 11:20:51 +0100

    Add BashRC

---
+++ .bash
@@ -0,0 +1,11 @@
+#
+# ~/.bashrc
+#
+
+# If not running interactively, don't do anything
+[[ $- != *i* ]] && return
+
+alias ls='ls --color=auto'
+# PS1='[\u@\h \W]\$ '
+PS1='$ '
+export NODE_PATH=/home/ghost/.npm-packages/lib/node_modules/home/ghost/.npm-packages/lib/node_modules:/home/ghost/.npm-packages/lib/node_modules
